6fefb10d1624d59e048df94fa2b13fa7aed9109f
abdul awal suhail <abdullahsuhail43@gmail.com>
2022-05-12 09:57:05 +0600

    delete fu

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config
-const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -10,25 +10,55 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.ciqeb.mongodb.net:27017,cluster0-shard-00-01.ciqeb.mongodb.net:27017,cluster0-shard-00-02.ciqeb.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-35hjy9-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }   );
+
+async function run() {
+    try {
+        await client.connect()
+        const manageCollection = client.db('WareHouse').collection('ManageItem')
 
 
+        app.get('/item', async (req, res) => {
+            const query = {}
+            const cursor = manageCollection.find(query)
+            const manages = await cursor.toArray()
+            res.send(manages)
+        })
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await manageCollection.findOne(query);
+            res.send(service);
+        });
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@services.q9saz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("WareHouse").collection("ManageItem");
-  console.log('connected')
-  // perform actions on the collection object
-  client.close();
-});
+        // POST
+        app.post('/item', async (req, res) => {
+            const newService = req.body;
+            const result = await manageCollection.insertOne(newService);
+            res.send(result);
+        });
+
+        // DELETE
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await manageCollection.deleteOne(query);
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
-    res.send('Running Genius Server');
+    res.send('Running WareHouse Server');
 });
-
-app.get('/hero', (req, res) =>{
-    res.send('Hero meets hero ku')
-})
 
 app.listen(port, () => {
     console.log('Listening to port', port);
